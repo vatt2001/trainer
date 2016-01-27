69adf8b978836c8058f0b89d48c7e8c1a1738cba
Artem <vatt2001@gmail.com>
2016-01-27 19:58:49 +0300

    Added UserModel

--- src/main/scala/ru/art0/trainer/models/User.scala
+++ src/main/scala/ru/art0/trainer/models/User.scala
@@ -1,3 +1,21 @@
 package ru.art0.trainer.models
 
-case class User(id: Int, email: String, password: String, isActive: Boolean)
+case class User(id: Int, email: String, passwordHash: String, isActive: Boolean)
+
+trait UserModel extends AbstractModel {
+
+  import databaseProfile.simple._
+
+  class UserTable(tag: Tag) extends Table[User](tag, "user") {
+    def id = column[Int]("id", O.PrimaryKey)
+    def email = column[Int]("email")
+    def passwordHash = column[Int]("passwordHash")
+    def isActive = column[Boolean]("is_active")
+
+    def * = (id, email, passwordHash, isActive) <> (User.tupled, User.unapply)
+
+    def emailIdx = index("idx_user__email", email, unique = true)
+  }
+
+  val users = TableQuery[User]
+}
