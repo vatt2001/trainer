6f892be22b8ebaba653eabfdee423be86d4ba67d
Artem <vatt2001@gmail.com>
2016-01-27 20:00:58 +0300

    Added WordModel

--- src/main/scala/ru/art0/trainer/models/Word.scala
+++ src/main/scala/ru/art0/trainer/models/Word.scala
@@ -2,7 +2,23 @@ package ru.art0.trainer.models
 
 case class Word(id: Int, spelling: String, translation: String, transcription: String)
 
+trait WordModel extends AbstractModel {
 
+  import databaseProfile.simple._
+
+  class WordTable(tag: Tag) extends Table[Word](tag, "word") {
+    def id = column[Int]("id", O.PrimaryKey)
+    def spelling = column[String]("spelling")
+    def translation = column[String]("translation")
+    def transcription = column[String]("transcription")
+
+    def * = (id, spelling, translation, transcription) <> (Word.tupled, Word.unapply)
+
+    def spellingIdx = index("idx_word__spelling", spelling, unique = false)
+  }
+
+  val words = TableQuery[Word]
+}
 
 
 
